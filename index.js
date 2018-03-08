f75b1418ace5c53ab5df0aa5cc687bc71b4e3175
Ryan Hu <ryanhuuu@gmail.com>
2018-03-08 02:51:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+count =0;
+
 function takeANumber(deli, name){
-   deli.push(name);
-   return "Welcome, " + name + ". You are number " + deli.length  + " in line."
+   //deli.push(name);
+   //return "Welcome, " + name + ". You are number " + deli.length  + " in line."
+   Time.now
+   time = "hours.minutes";
+   ++count;
+   deli.push(count);
+   log.push(time);
+   return "Welcome, " + name + ". You are number " + count  + " in line."
+   
+   take a number keeps pushing in the nnumber, not shifting 
    
 }
 
-function nowServing(deli){
+function nowServing(deli,vare){
   if (deli[0]){
-    return "Currently serving " + deli.shift() + "."
+    return "Currently serving " + deli[vare] + "."
   }
   return "There is nobody waiting to be served!";
 }
@@ -20,3 +30,6 @@ function currentLine(deli){
    var count = 1;
    return "The line is currently: 1. " + deli.reduce((acum, curval) => acum + ", " + (++count) + ". " + curval);
 }
+
+
+
